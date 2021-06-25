e64194b0bc786cc589be7d442e1a011cd1f839b9
luchosr <luchosr@gmail.com>
2021-06-25 20:38:07 +0200

    update websocket connection

--- src/components/StockRow.js
+++ src/components/StockRow.js
@@ -1,12 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const StockRow = ({ ticker }) => {
   const [price, setPrice] = useState(5);
   const [date, setDate] = useState("12-12-12");
   const [time, setTime] = useState("09:45");
+  const ws = useRef(null);
 
+  // const ws = new WebSocket("wss://www.cryptofacilities.com/ws/v1");
   //fetch api
-  useEffect(() => {}, []);
+  useEffect(() => {
+    ws.current = new WebSocket("wss://www.cryptofacilities.com/ws/v1");
+    ws.current.onopen = () => console.log("ws opened");
+    ws.current.onclose = () => console.log("ws closed");
+
+    return () => {
+      ws.current.close();
+    };
+  }, []);
 
   return (
     <tr>
